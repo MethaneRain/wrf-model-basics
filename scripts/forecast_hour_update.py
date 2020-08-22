632c6d89e9984a9a3b896b1a2fbc69c811456b62
MethaneRain <justin.richling@gmail.com>
2020-08-22 17:15:24 +0000

    Update forecast_hour_update.py

--- scripts/forecast_hour_update.py
+++ scripts/forecast_hour_update.py
@@ -1,4 +1,5 @@
-
+# Script to add appropriate leading zeros. 
+# Currently defaulted to GFS filenames which have 3 digits for forecast hours
 import sys
 f_hour = sys.argv[1].zfill(3)
 sys.stdout.write(f_hour)
