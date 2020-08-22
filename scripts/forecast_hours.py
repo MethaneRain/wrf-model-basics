37e3fe2721c6914ae3e84035414a931a482ccef8
MethaneRain <justin.richling@gmail.com>
2020-08-22 16:52:26 +0000

    Update forecast_hours.py

--- scripts/forecast_hours.py
+++ scripts/forecast_hours.py
@@ -1,10 +1,13 @@
+# This script will take the input time delta and split it into forecast steps for specified model
+# As of right now, the GFS is the only model currently used, so the forecast runs are every 3 hours
+
 import sys
 delta = sys.argv[1]
 
-ass = [i for i in range(0,int(delta)+1,3)]
+time_delta = [i for i in range(0,int(delta)+1,3)]
 
 returnStr = ''
-for item in ass:
+for item in time_delta:
     returnStr += str(item)+' '
 
 sys.stdout.write(returnStr)
